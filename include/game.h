4e3d19e29e1f98b1987ac82c98a95e177e572bcd
Roy Theimer <roy.theimer@gmail.com>
2024-06-26 10:07:19 +0200

    Add foodOnSnake function
    added the foodOnSnake function to check wether they overlap, changed
    some comments

--- include/game.h
+++ include/game.h
@@ -6,14 +6,17 @@
 // Generate a random position on the grid based on cell size and grid dimensions
 Vector2 getPosition(int cellSize, int cols, int rows);
 
-// check for Border collision
+// check for Snake Border collision
 bool borderCollision(Node *head, int screenWidth, int screenHeight,
                      int cellSize);
 
-// check Food Collision
+// check Snake Head Food Collision
 bool foodCollision(Node *head, Rectangle *rect);
 
-// check Body Collision
+// check Snake Head Body Collision
 bool bodyCollision(Node *head);
+
+// check Food placed on Snake
+bool foodOnSnake(Node *head, Rectangle *rect);
 
 #endif
